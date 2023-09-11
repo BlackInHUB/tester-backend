34509fff19e3b9675573c08e99570da00a10573e
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-09-11 08:24:32 +0300

    Update for getTest endpoint

--- src/controllers/tests/getTest.js
+++ src/controllers/tests/getTest.js
@@ -1,10 +1,21 @@
 const {Test} = require('../../models');
+const mongoose = require('mongoose');
 
 const getTest = async (req, res) => {
     const {_id} = req.params;
     
-    const test = await Test.findById(_id);
-    
+    const test = await Test.aggregate([
+        { "$match": { _id: new mongoose.Types.ObjectId(_id)}},
+        {$project: {
+        author: 1,
+        options: 1,
+        category: 1,
+        questions: 1,
+        passedUsers: {$size: "$passedUsers"},
+        maxScore: {$max: "$results.score"},
+        bestTime: {$min: "$results.time"}
+    }}]);
+
     res.status(200).json(test);
 };
 
