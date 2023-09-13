86fbe78d85cd1b34af1af7013c4163d6711b6bb0
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-09-13 18:31:41 +0300

    Fix create endpoint response

--- src/controllers/tests/create.js
+++ src/controllers/tests/create.js
@@ -1,16 +1,20 @@
-const {Test} = require('../../models');
+const {Test, User} = require('../../models');
 
 const create = async (req, res) => {
     const {_id: author} = req.user;
 
     const result = await Test.create({author, ...req.body});
 
+    await User.populate(result, {path: 'author', select: ('name')});
+
     return res.status(201).json({
         author: result.author,
         _id: result._id,
         questions: result.questions.length,
         options: result.options,
-        category: result.category
+        category: result.category,
+        passedUsers: result.passedUsers.length,
+        results: result.results.length,
     });
 };
 
