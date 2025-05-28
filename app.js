e51e7f5948d67f64794dee92d81f3ebaf715a16a
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-05-28 11:06:54 +0300

    cooors..

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const corsOptions = {
   origin: 'https://cozy-lokum-790cda.netlify.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
